4edfa5f767a616942d8b73f13dcd53c64ffc5649
asadaligreelogix <asad.ali@greelogix.com>
2022-08-04 11:23:35 +0500

    added sapces in index and register

--- register.php
+++ register.php
@@ -18,6 +18,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Foss Ball</title>
+    
     <script src="https://common.olemiss.edu/_js/sweet-alert/sweet-alert.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://common.olemiss.edu/_js/sweet-alert/sweet-alert.css">
      
